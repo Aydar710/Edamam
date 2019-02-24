720d1c5e05a12ed48a642e5529bb2b256851c20a
kehtolaulu <emilia270627@gmail.com>
2019-02-24 18:50:00 +0300

    add method to repository

--- app/src/main/java/com/m/edamam/repositories/RecipeRepository.kt
+++ app/src/main/java/com/m/edamam/repositories/RecipeRepository.kt
@@ -1,5 +1,6 @@
 package com.m.edamam.repositories
 
+import com.m.edamam.pojo.Recipe
 import com.m.edamam.pojo.RecipesResponse
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
@@ -9,6 +10,10 @@ class RecipeRepository(val api : EdamamApi) {
     fun getRecipesByName(query : String): Single<RecipesResponse>? {
             return api.getRecipesByName(query)
                     .subscribeOn(Schedulers.io())
+    }
+
+    fun getRecipeById(query: String?): Single<Recipe>? {
+        return api.getRecipeById(query).subscribeOn(Schedulers.io())
     }
 
 }
