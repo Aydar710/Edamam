f650197ff6b1873276ac13c93b4cdcf192342160
Aydar <aydar710@gmail.com>
2019-03-19 18:28:53 +0300

    checked detekt

--- app/src/main/java/com/m/edamam/di/component/PresenterComponent.kt
+++ app/src/main/java/com/m/edamam/di/component/PresenterComponent.kt
@@ -1,12 +1,9 @@
 package com.m.edamam.di.component
 
-import com.m.edamam.di.module.NetModule
 import com.m.edamam.di.module.PresenterModule
-import com.m.edamam.di.module.ServiceModule
 import com.m.edamam.presenters.DetailsFragmentPresenter
 import com.m.edamam.presenters.RecipeListFragmentPresenter
 import com.m.edamam.presenters.RecommendationFragmentPresenter
-import com.m.edamam.ui.RecipeListFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -16,5 +13,4 @@ interface PresenterComponent {
     fun getRecipeListFragmentPresenter(): RecipeListFragmentPresenter
     fun getDetailsFragmentPresenter(): DetailsFragmentPresenter
     fun getRecommendationFragmentPresenter(): RecommendationFragmentPresenter
-    //fun inject(fragment : RecipeListFragment)
 }
