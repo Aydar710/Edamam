cc6e4babf7eab4f69456c875625fa891892d0599
Aydar <aydar710@gmail.com>
2019-03-22 13:18:16 +0300

    check detekt

--- app/src/main/java/com/m/edamam/di/module/AppModule.kt
+++ app/src/main/java/com/m/edamam/di/module/AppModule.kt
@@ -7,7 +7,7 @@ import javax.inject.Singleton
 
 @Module
 class AppModule(val context: Context) {
-    
+
     @Provides
     @Singleton
     fun provideContext(): Context = context.applicationContext
